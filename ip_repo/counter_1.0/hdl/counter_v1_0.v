ae5f9318473c6079752f794ef716a9eb133af824
bop-hz <bo.peng@blueye.no>
2017-06-24 16:08:52 +0200

    next step

---
+++ ip_repo/counter_1.0/hdl/counter_v1_0.v
@@ -0,0 +1,53 @@
+
+`timescale 1 ns / 1 ps
+
+	module counter_v1_0 #
+	(
+		// Users to add parameters here
+
+		// User parameters ends
+		// Do not modify the parameters beyond this line
+
+
+		// Parameters of Axi Master Bus Interface M00_AXIS
+		parameter integer C_M00_AXIS_TDATA_WIDTH	= 32,
+		parameter integer C_M00_AXIS_START_COUNT	= 32
+	)
+	(
+		// Users to add ports here
+        input EN,
+        input [7:0] Frame_size,
+		// User ports ends
+		// Do not modify the ports beyond this line
+
+
+		// Ports of Axi Master Bus Interface M00_AXIS
+		input wire  m00_axis_aclk,
+		input wire  m00_axis_aresetn,
+		output wire  m00_axis_tvalid,
+		output wire [C_M00_AXIS_TDATA_WIDTH-1 : 0] m00_axis_tdata,
+		output wire [(C_M00_AXIS_TDATA_WIDTH/8)-1 : 0] m00_axis_tstrb,
+		output wire  m00_axis_tlast,
+		input wire  m00_axis_tready
+	);
+// Instantiation of Axi Bus Interface M00_AXIS
+	counter_v1_0_M00_AXIS # ( 
+		.C_M_AXIS_TDATA_WIDTH(C_M00_AXIS_TDATA_WIDTH),
+		.C_M_START_COUNT(C_M00_AXIS_START_COUNT)
+	) counter_v1_0_M00_AXIS_inst (
+	   // .EN(EN),
+	  //  .Frame_size(Frame_size),
+		.M_AXIS_ACLK(m00_axis_aclk),
+		.M_AXIS_ARESETN(m00_axis_aresetn),
+		.M_AXIS_TVALID(m00_axis_tvalid),
+		.M_AXIS_TDATA(m00_axis_tdata),
+		.M_AXIS_TSTRB(m00_axis_tstrb),
+		.M_AXIS_TLAST(m00_axis_tlast),
+		.M_AXIS_TREADY(m00_axis_tready)
+	);
+
+	// Add user logic here
+
+	// User logic ends
+
+	endmodule
